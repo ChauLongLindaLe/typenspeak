9f324c22db45c4c4fa8fccfbf8e6d6b33ce90eab
Linda Le <linchowmein@hotmail.com>
2018-09-10 12:54:26 +0900

    voices array now contains the varying voices from the API

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-console.log('hello');
+//Init SpeechSynth API
+const synth = window.speechSynthesis;
+
+//Grabbing all DOM elements
+const textForm = document.querySelector('form');
+const textInput = document.querySelector('#text-input');
+const voiceSelect = document.querySelector('#voice-select');
+const rate = document.querySelector('#rate');
+const rateValue = document.querySelector('#rate-value');
+const pitch = document.querySelector('#pitch');
+const pitchValue = document.querySelector('#pitch-value');
+
+
+//init voices array. Fetch the voices using the API
+let voices =[]
+
+const getVoices = () => {
+  voices = synth.getVoices();
+  //this is called asynchronously so we need to wait for onvoiceschanged event is fired when it is loaded. To get around this we set the voice from the callback for that event listener
+  console.log(voices);
+  // reaches into the API and it has a method getvoices
+  //
+};
+
+getVoices();
+
+if(synth.onvoiceschanged !== undefined){
+  synth.onvoiceschanged = getVoices;
+}
