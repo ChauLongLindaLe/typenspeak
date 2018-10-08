75698a919179c0596287ff23ec2f06717292a470
Linda Le <linchowmein@hotmail.com>
2018-10-08 21:07:45 +0900

    bug fixing

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ const pitchValue = document.querySelector('#pitch-value');
 
 
 //init voices array. Fetch the voices using the API
-let voices =[]
+let voices =[];
 
 const getVoices = () => {
   voices = synth.getVoices();
@@ -23,11 +23,67 @@ const getVoices = () => {
 
 
   //Loop through voices and create option for each one
+  voices.forEach(voice => {
+    //create option elements
+    const option = document.createElement('option');
+    //Fill option with voice and language
+    option.textContent = voice.name + '('+ voice.lang +')';
 
-
+    //Set needed option attributes
+    option.setAttribute('data-lang, voice.lang');
+    option.setAttribute('data-name, voice.name');
+    voiceSelect.appendChild(option);
+  });
 };
 
 getVoices();
 if(synth.onvoiceschanged !== undefined){
   synth.onvoiceschanged = getVoices;
 }
+
+//Speak
+
+const speak = () => {
+  //check if speaking
+  if(synth.speaking){
+    console.error('Already speaking..');
+    return;
+  }
+  if(textInput.value !== ''){
+
+    //Get speak text
+    const speakText = new SpeechSynthesisUtterance(textInput.value);
+    //Speak end
+
+    speakText.onend = e => {
+      console.log('Done speaking...');
+    };
+
+    //Speak error
+    speakText.onerror = e => {
+      console.error('Something went wrong');
+
+    };
+
+    //Selected voice
+
+    const selectedVoice = voiceSelect.selectedOptions[0]
+      .getAttribute('data-name');
+
+    //Loop through voices
+    voices.forEach(voice => {
+      if(voice.name === selectedVoice){
+        speakText.voice =voice;
+      }
+    });
+
+    //Set pitch and rate
+    speakText.rate = rate.value;
+    speakText.pitch = pitch.value;
+
+    //Speak
+    synth.speak(speakText);
+  }
+};
+
+//EVENT LISTENERS
